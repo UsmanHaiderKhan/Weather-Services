c5f7b06fe851a387d76f1900a82fc7b91199b705
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-02 15:57:48 +0500

    Update Added Waether Js Action Method in the home Controller

--- Weather Services/Controllers/HomeController.cs
+++ Weather Services/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Weather_Services.Controllers
 {
@@ -10,6 +6,11 @@ namespace Weather_Services.Controllers
     {
         // GET: Home
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult WeatherJs()
         {
             return View();
         }
